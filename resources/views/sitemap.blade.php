10ddaff5a4a2b213279b2014790abe1a5afff673
niklavsk <niklavsk36@gmail.com>
2022-04-09 15:34:09 +0300

    Update sitemap.blade.php

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -6,27 +6,31 @@
             http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
     <url>
         <loc>https://kadikis.id.lv/</loc>
-        <lastmod>2022-03-30T16:30:41+00:00</lastmod>
+        <lastmod>2022-04-09T12:30:46+00:00</lastmod>
         <priority>1.00</priority>
     </url>
     <url>
-        <loc>https://kadikis.id.lv/interesting-places-to-see-stray-cats</loc>
-        <lastmod>2022-03-30T16:30:41+00:00</lastmod>
+        <loc>
+            https://kadikis.id.lv/interesting-places-to-see-stray-cats
+        </loc>
+        <lastmod>2022-04-09T12:30:46+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
         <loc>https://kadikis.id.lv/cat-cafes</loc>
-        <lastmod>2022-03-30T16:30:41+00:00</lastmod>
+        <lastmod>2022-04-09T12:30:46+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
         <loc>https://kadikis.id.lv/how-to-care-for-street-cats</loc>
-        <lastmod>2022-03-30T16:30:41+00:00</lastmod>
+        <lastmod>2022-04-09T12:30:46+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
-        <loc>https://kadikis.id.lv/how-to-introduce-a-new-cat-to-other-cats-at-home</loc>
-        <lastmod>2022-03-30T16:30:41+00:00</lastmod>
+        <loc>
+            https://kadikis.id.lv/how-to-introduce-a-new-cat-to-other-cats-at-home
+        </loc>
+        <lastmod>2022-04-09T12:30:46+00:00</lastmod>
         <priority>0.80</priority>
     </url>
 </urlset>
